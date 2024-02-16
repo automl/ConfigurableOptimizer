4f95a97e6d75c355381db4770658ab03de214734
Shakiba Moradian <moradias@informatik.uni-freiburg.de>
2024-02-16 13:12:57 +0100

    test(test_utils): add unit tests for the refactored logger and fix test_prepare_logger based on the refactor

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,7 +13,7 @@ class TestUtils(unittest.TestCase):
     def test_prepare_logger(self) -> None:
         save_dir = Path(".") / "tests" / "logs"
         exp_name = "test_exp"
-        logger = prepare_logger(save_dir=save_dir, seed=9001, exp_name=exp_name)
+        logger = prepare_logger(save_dir=str(save_dir), seed=9001, exp_name=exp_name)
         checkpoints_path = logger.path(mode="checkpoints")
         assert os.path.exists(checkpoints_path)
         assert os.path.exists(logger.logger_path)
@@ -42,6 +42,180 @@ class TestUtils(unittest.TestCase):
         shuffled_x = channel_shuffle(x, groups=k)
         assert x.shape == shuffled_x.shape
         assert not torch.allclose(original_x, shuffled_x)
+
+
+class TestLogger(unittest.TestCase):
+    def test_logger_init_with_run_time(self) -> None:
+        log_dir = str(Path(".") / "tests" / "logs")
+        logger_source = Logger(
+            log_dir=log_dir,
+            exp_name="testiiiing",
+            search_space="darts",
+            dataset="cifar100",
+            seed="12",
+            use_supernet_checkpoint=False,
+        )
+        expr_path = "/".join(
+            [
+                log_dir,
+                "testiiiing",
+                "darts",
+                "cifar100",
+                "12",
+                "discrete",
+            ]
+        )
+        logger = Logger(
+            log_dir=log_dir,
+            exp_name="testiiiing",
+            search_space="darts",
+            dataset="cifar100",
+            seed="12",
+            run_time=logger_source.run_time,
+            use_supernet_checkpoint=False,
+        )
+        assert logger_source.run_time == logger.run_time
+        assert os.path.exists("/".join([expr_path, logger.run_time, "log"]))
+
+    def test_logger_init_with_last_run(self) -> None:
+        log_dir = str(Path(".") / "tests" / "logs")
+        logger_source = Logger(
+            log_dir=log_dir,
+            exp_name="testiiiing",
+            search_space="darts",
+            dataset="cifar100",
+            seed="12",
+            use_supernet_checkpoint=False,
+        )
+        expr_path = "/".join(
+            [
+                log_dir,
+                "testiiiing",
+                "darts",
+                "cifar100",
+                "12",
+                "discrete",
+            ]
+        )
+        logger = Logger(
+            log_dir=log_dir,
+            exp_name="testiiiing",
+            search_space="darts",
+            dataset="cifar100",
+            seed="12",
+            use_supernet_checkpoint=False,
+            last_run=True,
+        )
+        assert logger_source.run_time == logger.run_time
+        assert os.path.exists("/".join([expr_path, logger.run_time, "log"]))
+
+    def test_logger_init(self) -> None:
+        log_dir = str(Path(".") / "tests" / "logs")
+        expr_path = "/".join(
+            [
+                log_dir,
+                "testiiiing",
+                "darts",
+                "cifar100",
+                "12",
+                "discrete",
+            ]
+        )
+        logger = Logger(
+            log_dir=log_dir,
+            exp_name="testiiiing",
+            search_space="darts",
+            dataset="cifar100",
+            seed="12",
+            use_supernet_checkpoint=True,
+        )
+        assert os.path.exists(expr_path)
+        assert os.path.exists("/".join([expr_path, "last_run"]))
+        assert os.path.exists("/".join([expr_path, logger.run_time, "log"]))
+        assert os.path.exists("/".join([expr_path, logger.run_time, "checkpoints"]))
+
+    # def test_set_up_new_run(self) -> None:
+    #     ...
+
+    # def test_set_up_run(self) -> None:
+    #     ...
+
+    def test_expr_log_path(self) -> None:
+        log_dir = str(Path(".") / "tests" / "logs")
+        expr_path = "/".join(
+            [
+                log_dir,
+                "testiiiing",
+                "darts",
+                "cifar100",
+                "12",
+                "discrete",
+            ]
+        )
+        logger = Logger(
+            log_dir=log_dir,
+            exp_name="testiiiing",
+            search_space="darts",
+            dataset="cifar100",
+            seed="12",
+            use_supernet_checkpoint=False,
+        )
+        assert logger.expr_log_path() == Path(expr_path)
+
+    def test_load_last_run(self) -> None:
+        log_dir = str(Path(".") / "tests" / "logs")
+        "/".join(
+            [
+                log_dir,
+                "testiiiing",
+                "darts",
+                "cifar100",
+                "12",
+                "discrete",
+            ]
+        )
+        logger = Logger(
+            log_dir=log_dir,
+            exp_name="testiiiing",
+            search_space="darts",
+            dataset="cifar100",
+            seed="12",
+            use_supernet_checkpoint=False,
+        )
+        assert logger.load_last_run() == logger.run_time
+
+    # def test_save_last_run(self) -> None:
+    #     ...
+
+    def test_path(self) -> None:
+        log_dir = str(Path(".") / "tests" / "logs")
+
+        logger = Logger(
+            log_dir=log_dir,
+            exp_name="testiiiing",
+            search_space="darts",
+            dataset="cifar100",
+            seed="12",
+            use_supernet_checkpoint=False,
+        )
+        expr_path = "/".join(
+            [
+                log_dir,
+                "testiiiing",
+                "darts",
+                "cifar100",
+                "12",
+                "discrete",
+                logger.run_time,
+            ]
+        )
+        assert logger.path("best_model") == "/".join([expr_path, "best_model.pth"])
+        assert logger.path("checkpoints") == "/".join([expr_path, "checkpoints"])
+        assert logger.path("log") == "/".join([expr_path, "log"])
+        # assert logger.path("last_checkpoint")=='/'.join([expr_path, "best_model.pth"])
+
+    # def test_log(self) -> None:
+    #     ...
 
 
 if __name__ == "__main__":
