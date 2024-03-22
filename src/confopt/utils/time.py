5a154a7fd8f19de5c1fb323b1ff8e83f5640ca3d
Shakiba Moradian <moradias@informatik.uni-freiburg.de>
2024-03-22 15:56:29 +0100

    feat(utils/time.py): add a function that would check the runtime would have the correct format

--- src/confopt/utils/time.py
+++ src/confopt/utils/time.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import re
 import time
 
 
@@ -7,3 +8,8 @@ def get_time_as_string() -> str:
     """Gets the current date and time as a string."""
     time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     return time_str
+
+
+def check_date_format(input_string: str) -> bool:
+    pattern = r"^\d{4}-\d{2}-[A-Za-z]{3}-\d{2}:\d{2}:\d{2}$"
+    return re.match(pattern, input_string) is not None
