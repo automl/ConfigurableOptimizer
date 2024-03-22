c2f0fe88c29759b4b1dc1859f9b95a60f8021ae5
Shakiba Moradian <moradias@informatik.uni-freiburg.de>
2024-03-22 17:09:47 +0100

    fix(searchspace/__init__.py): add imports of darts and nb201 models

--- src/confopt/searchspace/__init__.py
+++ src/confopt/searchspace/__init__.py
@@ -1,6 +1,10 @@
 from .common.base_search import SearchSpace
+from .darts.core import DARTSImageNetModel, DARTSModel  # type: ignore
+from .darts.core.genotypes import Genotype as DARTSGenotype
 from .darts.supernet import DARTSSearchSpace  # type: ignore
 from .nb1shot1.supernet import NASBench1Shot1SearchSpace  # type: ignore
+from .nb201.core import NASBench201Model  # type: ignore
+from .nb201.core.genotypes import Structure as NAS201Genotype
 from .nb201.supernet import NASBench201SearchSpace  # type: ignore
 from .tnb101.supernet import TransNASBench101SearchSpace  # type: ignore
 
@@ -10,4 +14,9 @@ __all__ = [
     "NASBench1Shot1SearchSpace",
     "TransNASBench101SearchSpace",
     "SearchSpace",
+    "DARTSModel",
+    "DARTSImageNetModel",
+    "DARTSGenotype",
+    "NASBench201Model",
+    "NAS201Genotype",
 ]
