91430ac112cef9b3d567d65bd6cd3b366526c554
gierle <leon.gieringer@gmx.de>
2024-04-05 12:12:13 +0200

    test(ReduceChannel): add tests for width increase

--- tests/test_reduce_channels.py
+++ tests/test_reduce_channels.py
@@ -6,6 +6,8 @@ from torch import nn
 from confopt.utils.reduce_channels import (
     reduce_bn_features,
     reduce_conv_channels,
+    increase_bn_features,
+    increase_conv_channels,
 )
 
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -16,6 +18,7 @@ class TestReduceChannels(unittest.TestCase):
         original_conv2d = nn.Conv2d(
             in_channels=6, out_channels=12, kernel_size=3, stride=1, padding=1
         ).to(DEVICE)
+        original_conv2d.weight.data.fill_(0.5)
 
         reduced_conv2d = reduce_conv_channels(original_conv2d, k=2, device=DEVICE)
 
@@ -35,6 +38,7 @@ class TestReduceChannels(unittest.TestCase):
 
     def test_reduce_features(self) -> None:
         original_batchnorm = nn.BatchNorm2d(num_features=12).to(DEVICE)
+        original_batchnorm.weight.data.fill_(0.5)
 
         reduced_batchnorm = reduce_bn_features(original_batchnorm, k=3, device=DEVICE)
 
@@ -46,6 +50,46 @@ class TestReduceChannels(unittest.TestCase):
             )
             assert torch.all(
                 torch.eq(reduced_batchnorm.bias[:4], original_batchnorm.bias[:4])
+            )
+
+    def test_increase_conv_channels(self) -> None:
+        original_conv2d = nn.Conv2d(
+            in_channels=3, out_channels=6, kernel_size=3, stride=1, padding=1
+        ).to(DEVICE)
+        original_conv2d.weight.data.fill_(0.5)
+
+        increased_conv2d = increase_conv_channels(original_conv2d, k=0.5, device=DEVICE)
+
+        assert increased_conv2d.in_channels == 6
+        assert increased_conv2d.out_channels == 12
+
+        assert torch.all(
+            torch.eq(
+                original_conv2d.weight[:6, :3, :, :],
+                increased_conv2d.weight[:6, :3, :, :],
+            )
+        )
+        if original_conv2d.bias is not None:
+            assert torch.all(
+                torch.eq(original_conv2d.bias[:6], increased_conv2d.bias[:6])
+            )
+
+    def test_increase_features(self) -> None:
+        original_batchnorm = nn.BatchNorm2d(num_features=6).to(DEVICE)
+        original_batchnorm.weight.data.fill_(0.5)
+
+        reduced_batchnorm = increase_bn_features(
+            original_batchnorm, k=0.5, device=DEVICE
+        )
+
+        assert reduced_batchnorm.num_features == 12
+
+        if original_batchnorm.affine:
+            assert torch.all(
+                torch.eq(reduced_batchnorm.weight[:6], original_batchnorm.weight[:6])
+            )
+            assert torch.all(
+                torch.eq(reduced_batchnorm.bias[:6], original_batchnorm.bias[:6])
             )
 
 
