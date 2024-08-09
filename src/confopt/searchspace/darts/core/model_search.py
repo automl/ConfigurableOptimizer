fix: fix bug in attention computation

src/confopt/searchspace/darts/core/model_search.py
@@ -669,8 +669,8 @@ class Network(nn.Module):
         self, normal_alphas: nn.Parameter, reduce_alphas: nn.Parameter
     ) -> tuple[torch.Tensor, torch.Tensor]:
         all_arch_params = torch.concat((normal_alphas, reduce_alphas))
-        all_arch_attn = self.multihead_attention(
-            all_arch_params, all_arch_params, all_arch_params
+        all_arch_attn, _ = self.multihead_attention(
+            all_arch_params, all_arch_params, all_arch_params, need_weights=False
         )
         num_edges_normal = normal_alphas.shape[0]
         attn_normal = all_arch_attn[:num_edges_normal]

src/confopt/searchspace/tnb101/core/model_search.py
@@ -322,7 +322,7 @@ class TNB101SearchModel(nn.Module):
         return list(params)
 
     def _compute_arch_attention(self, alphas: nn.Parameter) -> torch.Tensor:
-        attn_alphas = self.multihead_attention(alphas, alphas, alphas)
+        attn_alphas, _ = self.multihead_attention(alphas, alphas, alphas)
         return attn_alphas
 
 