9b8518b4ea7d84aa73d21c686354a36cfb384874
abhash-er <abhashk2@outlook.com>
2024-08-20 17:09:11 +0200

    fix(Pruner): replace num_keep to prune_fraction

--- src/confopt/oneshot/pruner/pruner.py
+++ src/confopt/oneshot/pruner/pruner.py
@@ -11,12 +11,13 @@ class Pruner(OneShotComponent):
         self,
         searchspace: SearchSpace,
         prune_epochs: list[int],
-        prune_num_keeps: list[int],
+        prune_fractions: list[float],
     ) -> None:
         super().__init__()
         self.prune_epochs = prune_epochs
         self.searchspace = searchspace
         self.use_prune = True
+        # TODO Remove after merge of refactor PR or add a PruningSupport hook
         if not hasattr(searchspace, "prune"):
             warnings.warn(
                 f"The searchspace {type(searchspace)} does not have "
@@ -25,15 +26,15 @@ class Pruner(OneShotComponent):
             )
             self.use_prune = False
 
-        self.prune_epoch_to_num_keep = {}
-        assert len(prune_num_keeps) == len(prune_epochs)
+        self.prune_epoch_to_fraction = {}
+        assert len(prune_fractions) == len(prune_epochs)
         for idx, epoch in enumerate(prune_epochs):
-            self.prune_epoch_to_num_keep[epoch] = prune_num_keeps[idx]
+            self.prune_epoch_to_fraction[epoch] = prune_fractions[idx]
 
     def new_epoch(self) -> None:
         super().new_epoch()
         if self.use_prune and self._epoch in self.prune_epochs:
             # apply the pruning mask
             self.searchspace.prune(
-                num_keep=self.prune_epoch_to_num_keep.get(self._epoch)
+                prune_fraction=self.prune_epoch_to_fraction.get(self._epoch)
             )
