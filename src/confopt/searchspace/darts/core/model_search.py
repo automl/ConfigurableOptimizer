fix(DARTSSearchSpace/NB201SearchSpace): move saving weight gradients before cell forward
save grad would happen after mask

src/confopt/searchspace/darts/core/model_search.py
@@ -327,6 +327,10 @@ class Network(nn.Module):
         weights_normal = self.sample(weights_normal_to_sample)
         weights_reduce = self.sample(weights_reduce_to_sample)
 
+        if self.mask is not None:
+            weights_normal = normalize_params(weights_normal, self.mask[0])
+            weights_reduce = normalize_params(weights_reduce, self.mask[1])
+
         return weights_normal, weights_reduce
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
@@ -355,13 +359,9 @@ class Network(nn.Module):
             if cell.reduction:
                 weights = weights_reduce.clone()
                 self.save_weight_grads(weights, cell_type="reduce")
-                if self.mask is not None:
-                    weights = normalize_params(weights, self.mask[1])
             else:
                 weights = weights_normal.clone()
                 self.save_weight_grads(weights, cell_type="normal")
-                if self.mask is not None:
-                    weights = normalize_params(weights, self.mask[0])
 
             s0, s1 = s1, cell(s0, s1, weights)
 
@@ -385,8 +385,6 @@ class Network(nn.Module):
             if cell.reduction:
                 weights = weights_reduce.clone()
                 self.save_weight_grads(weights, cell_type="reduce")
-                if self.mask is not None:
-                    weights = normalize_params(weights, self.mask[1])
                 n = 3
                 start = 2
                 weights2 = F.softmax(self.betas_reduce[0:2], dim=-1)
@@ -399,8 +397,6 @@ class Network(nn.Module):
             else:
                 weights = weights_normal.clone()
                 self.save_weight_grads(weights, cell_type="normal")
-                if self.mask is not None:
-                    weights = normalize_params(weights, self.mask[0])
                 n = 3
                 start = 2
                 weights2 = F.softmax(self.betas_normal[0:2], dim=-1)

src/confopt/searchspace/nb201/core/model_search.py
@@ -271,6 +271,10 @@ class NB201SearchModel(nn.Module):
             weights_to_sample = self._compute_arch_attention(weights_to_sample)
 
         weights = self.sample(weights_to_sample)
+
+        if self.mask is not None:
+            weights = normalize_params(weights, self.mask)
+
         return weights
 
     def save_gradient(self) -> Callable:
@@ -309,10 +313,7 @@ class NB201SearchModel(nn.Module):
         for _i, cell in enumerate(self.cells):
             if isinstance(cell, SearchCell):
                 alphas = weights.clone()
-                # Retain Grads for weights
                 self.save_weight_grads(alphas)
-                if self.mask is not None:
-                    alphas = normalize_params(alphas, self.mask)
                 feature = cell(feature, alphas)
             else:
                 feature = cell(feature)
@@ -348,8 +349,6 @@ class NB201SearchModel(nn.Module):
                     )
                     betas = torch.cat([betas, beta_node_v], dim=0)
 
-                if self.mask is not None:
-                    alphas = normalize_params(alphas, self.mask)
                 feature = cell(feature, alphas, betas)
             else:
                 feature = cell(feature)